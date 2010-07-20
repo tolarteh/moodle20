8e8de301c866707eebbd992d344effc0f9100a21
Federico Builes <federico.builes@gmail.com>
2010-07-20 17:41:41 -0500

    Update database scripts and migration XML.

--- mod/reservations/db/upgrade.php
+++ mod/reservations/db/upgrade.php
@@ -46,99 +46,29 @@ function xmldb_reservations_upgrade($oldversion=0) {
 
     $result = true;
 
-/// And upgrade begins here. For each one, you'll need one
-/// block of code similar to the next one. Please, delete
-/// this comment lines once this file start handling proper
-/// upgrade code.
+    if ($result && $oldversion < 20100720) {
 
-/// if ($result && $oldversion < YYYYMMDD00) { //New version in version.php
-///     $result = result of "/lib/ddllib.php" function calls
-/// }
+        $table = new xmldb_table('reservations');
 
-/// Lines below (this included)  MUST BE DELETED once you get the first version
-/// of your module ready to be installed. They are here only
-/// for demonstrative purposes and to show how the reservations
-/// iself has been upgraded.
+    /// Adding fields to table reservations
+        $table->add_field('id', XMLDB_TYPE_INTEGER, null, null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('equipment_id', XMLDB_TYPE_INTEGER, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('date', XMLDB_TYPE_INTEGER, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('duration', XMLDB_TYPE_INTEGER, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('owner_id', XMLDB_TYPE_INTEGER, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('course_id', XMLDB_TYPE_INTEGER, null, null, null, null, null);
+        $table->add_field('created_at', XMLDB_TYPE_INTEGER, null, null, null, null, null);
 
-/// For each upgrade block, the file reservations/version.php
-/// needs to be updated . Such change allows Moodle to know
-/// that this file has to be processed.
+    /// Adding keys to table reservations
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
 
-/// To know more about how to write correct DB upgrade scripts it's
-/// highly recommended to read information available at:
-///   http://docs.moodle.org/en/Development:XMLDB_Documentation
-/// and to play with the XMLDB Editor (in the admin menu) and its
-/// PHP generation posibilities.
+    /// Conditionally launch create table for reservations
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
 
-/// First example, some fields were added to the module on 20070400
-    if ($result && $oldversion < 2007040100) {
-
-    /// Define field course to be added to reservations
-        $table = new XMLDBTable('reservations');
-        $field = new XMLDBField('course');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'id');
-    /// Launch add field course
-        $result = $result && add_field($table, $field);
-
-    /// Define field intro to be added to reservations
-        $table = new XMLDBTable('reservations');
-        $field = new XMLDBField('intro');
-        $field->setAttributes(XMLDB_TYPE_TEXT, 'medium', null, null, null, null, null, null, 'name');
-    /// Launch add field intro
-        $result = $result && add_field($table, $field);
-
-    /// Define field introformat to be added to reservations
-        $table = new XMLDBTable('reservations');
-        $field = new XMLDBField('introformat');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'intro');
-    /// Launch add field introformat
-        $result = $result && add_field($table, $field);
-    }
-
-/// Second example, some hours later, the same day 20070401
-/// two more fields and one index were added (note the increment
-/// "01" in the last two digits of the version
-    if ($result && $oldversion < 2007040101) {
-
-    /// Define field timecreated to be added to reservations
-        $table = new XMLDBTable('reservations');
-        $field = new XMLDBField('timecreated');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'introformat');
-    /// Launch add field timecreated
-        $result = $result && add_field($table, $field);
-
-    /// Define field timemodified to be added to reservations
-        $table = new XMLDBTable('reservations');
-        $field = new XMLDBField('timemodified');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'timecreated');
-    /// Launch add field timemodified
-        $result = $result && add_field($table, $field);
-
-    /// Define index course (not unique) to be added to reservations
-        $table = new XMLDBTable('reservations');
-        $index = new XMLDBIndex('course');
-        $index->setAttributes(XMLDB_INDEX_NOTUNIQUE, array('course'));
-    /// Launch add index course
-        $result = $result && add_index($table, $index);
-    }
-
-/// Third example, the next day, 20070402 (with the trailing 00), some inserts were performed, related with the module
-    if ($result && $oldversion < 2007040200) {
-    /// Add some actions to get them properly displayed in the logs
-        $rec = new stdClass;
-        $rec->module = 'reservations';
-        $rec->action = 'add';
-        $rec->mtable = 'reservations';
-        $rec->filed  = 'name';
-    /// Insert the add action in log_display
-        $result = insert_record('log_display', $rec);
-    /// Now the update action
-        $rec->action = 'update';
-        $result = insert_record('log_display', $rec);
-    /// Now the view action
-        $rec->action = 'view';
-        $result = insert_record('log_display', $rec);
-    }
+    /// assignment savepoint reached
+        upgrade_mod_savepoint($result, XXXXXXXXXX, 'assignment');
 
 /// And that's all. Please, examine and understand the 3 example blocks above. Also
 /// it's interesting to look how other modules are using this script. Remember that
