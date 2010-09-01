f7053e197a5d50cbb6232e3c4a7b54ebb7c3311f
Federico Builes <federico.builes@gmail.com>
2010-09-01 16:59:17 -0500

    New reservation.

--- mod/reservations/new.php
+++ mod/reservations/new.php
@@ -1,18 +1,18 @@
 <?php
- require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
- require_once(dirname(__FILE__).'/locallib.php');
- require_once(dirname(__FILE__).'/lib.php');
+   require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+   require_once(dirname(__FILE__).'/locallib.php');
+   require_once(dirname(__FILE__).'/lib.php');
 
 
- $PAGE->set_url('/mod/reservations/new.php');
- $PAGE->set_title(get_string("pagetitle", "reservations"));
+   $PAGE->set_url('/mod/reservations/new.php');
+$PAGE->set_title(get_string("pagetitle", "reservations"));
 // echo $OUTPUT->header();
 ?>
 
 <h2>Reservar Laboratorio</h2>
 
 <h3>Horario</h3>
-
+<form action="/reservation.php" method="POST">
 
 <p>
   <em>Fecha:</em>
@@ -22,18 +22,21 @@
      select_for_years();
      ?>
 </p>
-  <em>Hora:</em>
-  <?php
-     select_for_hours();
-     ?>
+<em>Hora:</em>
+<?php
+   select_for_hours();
+   ?>
 </p>
 
 <h3>Equipos</h3>
-
 <?php print_equipment(); ?>
 
+<p>
+<input type="submit" value="Reservar" />
+</p>
 
+</form>
 
 <?php
-// echo $OUTPUT->footer();
+   // echo $OUTPUT->footer();
 ?>
