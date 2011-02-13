06ca455957868ed7bbd99cf173a70df76b7a54f7
Federico Builes <federico.builes@gmail.com>
2011-02-13 17:24:43 -0500

    Add salt to the passwords.

--- mod/reservations/auth.php
+++ mod/reservations/auth.php
@@ -6,7 +6,7 @@ require_once(dirname(__FILE__).'/locallib.php');
 $username = $_POST["user"];
 $hash = $_POST["hash"];
 
-if(auth_user($username, $hash)) {
+if(auth_user($username, $hash + "some_very_long_secret!#A12345678901234567890!")) {
   echo "true";
 } else {
   echo "false";
